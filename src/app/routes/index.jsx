07e0a369b2ba64f1f294ce37324fa03652250966
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-08-01 03:10:45 +0700

    feat: update routing

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import DetailsView from "../../views/DetailsView";
+import GamesByGenreView from "../../views/GamesByGenreView";
 import HomeView from "../../views/HomeView";
 
 export const createRouter = () =>
@@ -6,5 +8,13 @@ export const createRouter = () =>
     {
       path: "/",
       element: <HomeView />,
+    },
+    {
+      path: "/game/:id",
+      element: <DetailsView />,
+    },
+    {
+      path: "/games/genre/:name",
+      element: <GamesByGenreView />,
     },
   ]);
