ed0a8e21b73c371d571ea71eac325c0391c64938
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-08-01 03:12:19 +0700

    feat: create new component home view

--- src/views/HomeView/index.jsx
+++ src/views/HomeView/index.jsx
@@ -1,15 +1,25 @@
 import useGetGames from "../../libs/queries/useGetGames";
+import Genres from "./Genres";
+import Hero from "./Hero";
+import NewReleases from "./NewReleases";
 import { HomeViewWrapper } from "./styles";
+import Trending from "./Trending";
 
 const HomeView = () => {
-  const { data } = useGetGames();
-
-  console.log(data);
+  const { data, isFetching } = useGetGames({
+    ordering: "-added",
+    page_size: 3,
+  });
 
   return (
-    <HomeViewWrapper>
-      <h1>home</h1>
-    </HomeViewWrapper>
+    <>
+      <Hero />
+      <HomeViewWrapper>
+        <Trending data={data?.data?.results} isLoading={isFetching} />
+        <NewReleases />
+        <Genres />
+      </HomeViewWrapper>
+    </>
   );
 };
 
--- src/views/HomeView/styles.js
+++ src/views/HomeView/styles.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
 export const HomeViewWrapper = styled.div`
-  padding: 5rem 10rem;
+  padding: 0 10rem;
+  margin-bottom: 8rem;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+
+  @media (max-width: 1024px) {
+    padding: 0 5rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 2rem;
+  }
 `;
