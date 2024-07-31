dd3536a9a79ba3b4ff803353b55be4f57fffea67
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-08-01 03:11:19 +0700

    feat: create new component wide card

--- src/components/WideCard/index.jsx
+++ src/components/WideCard/index.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import { WideCardWrapper } from "./styles";
 
-const WideCard = ({ title, bgImage }) => {
+const WideCard = ({ title, bgImage, onClick }) => {
   return (
-    <WideCardWrapper bgImage={bgImage}>
+    <WideCardWrapper $bgImage={bgImage} onClick={onClick}>
       <h2>{title}</h2>
     </WideCardWrapper>
   );
@@ -14,4 +14,5 @@ export default WideCard;
 WideCard.propTypes = {
   title: PropTypes.string,
   bgImage: PropTypes.string,
+  onClick: PropTypes.func,
 };
--- src/components/WideCard/styles.js
+++ src/components/WideCard/styles.js
@@ -1,6 +1,22 @@
-import { url } from "inspector";
 import styled from "styled-components";
 
 export const WideCardWrapper = styled.div`
-  background: ${(props) => url(props.bgImage)};
+  padding: 3rem;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${(props) => props.$bgImage});
+  background-size: cover;
+  border-radius: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 0.75rem;
+  text-align: center;
+  position: relative;
+  height: 170px;
+  background-position: center 0%;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
